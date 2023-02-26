2ab1d3624c4cddbcba8fa58fde867e44477e6175
Igor Moura <igor.moura254@hotmail.com>
2023-02-26 14:49:15 -0300

    Testing lambdas, actions, funcs, etc

---
+++ Delegates/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Delegates
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Delegate helloWorldDelegate = HelloWorld;
+            helloWorldDelegate.DynamicInvoke();
+
+            var action = WriteMessage;
+
+            action("Hello!");
+
+            ConfigurarFramework(options =>
+            {
+                options.UsarTalCoisa = true;
+                options.ConfigurarOutraCoisa();
+            });
+
+            // Lambda expression
+            var method = () => Console.WriteLine("Hellow World!");
+
+            // Lambda statement
+            var function = () =>
+            {
+                Console.WriteLine("Hellow World 1!");
+                Console.WriteLine("Hellow World 2!");
+            };
+
+            method();
+            function();
+        }
+
+        private static void HelloWorld()
+        {
+
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static void ConfigurarFramework(Action<FrameworkOptions> options)
+        {
+            // ...
+        }
+
+        public static void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class FrameworkOptions
+    {
+        public bool UsarTalCoisa { get; set; }
+
+        public void ConfigurarOutraCoisa()
+        {
+            Console.WriteLine($"Chamou {nameof(ConfigurarOutraCoisa)}!");
+        }
+    }
+}
